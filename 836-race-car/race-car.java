3e348e35ac5efa314aafcff2ed0ca85f72763c5d
santoshingale <58105046+santoshingale@users.noreply.github.com>
2023-08-26 18:17:41 +0530

    Time: 34 ms (48.30%) | Memory: 43.7 MB (49.70%) - LeetSync

---
+++ 836-race-car/race-car.java
@@ -0,0 +1,28 @@
+class Solution {
+    List<Integer[]> queue = new ArrayList<Integer[]>(); 
+    HashSet<Integer[]> visited = new HashSet<Integer[]>(); 
+    int moves, position, speed = 0;
+    public int racecar(int target) {
+        queue.add(new Integer[]{0,0,1});
+        while(queue.size() > 0) {
+            moves = queue.get(0)[0];
+            position = queue.get(0)[1];
+            speed = queue.get(0)[2];
+            queue.remove(0);
+            
+            if (position == target) {
+                return moves;
+            } else if (visited.contains(new Integer[]{position, speed})) {
+                continue;
+            }
+            else {
+                visited.add(new Integer[]{position, speed});
+                queue.add(new Integer[]{moves+1,position+speed,speed*2 });
+                if ((position + speed > target && speed > 0) || ( position + speed < target && speed < 0)) {
+                    queue.add(new Integer[]{moves+1,position, speed > 0? -1: 1 });
+                }
+            }
+        }
+        return moves;
+    }
+}
