c1cbfd944f3f527ceda202b94699a2c555897c4c
santoshingale <58105046+santoshingale@users.noreply.github.com>
2024-02-21 19:24:47 +0530

    Time: 1 ms (74.73%) | Memory: 42.1 MB (64.58%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(!(str1+str2).equals(str2+str1)) return "";
+        int gcdval=gcd(str1.length(),str2.length());
+        return str2.substring(0,gcdval);
+    }
+
+    int gcd(int x,int y){
+      if(y==0) return x;
+      else return gcd(y,x%y);
+    }
+}
