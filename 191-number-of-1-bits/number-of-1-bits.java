1844907c0a8502d736b002713bf16581e52b6428
santoshingale <58105046+santoshingale@users.noreply.github.com>
2024-08-18 18:38:41 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (70.60%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -1,11 +1,17 @@
 public class Solution {
-    // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int ones = 0;
     	while(n!=0) {
-    		ones = ones + (n & 1);
-    		n = n>>>1;
+    		int rmsb = n & (-n);
+    		n -= rmsb;
+            ones++;
     	}
     	return ones;
     }
 }
+
+// note i have use Kernighan's Algorithm
+// ex : 11
+// binary : 1011
+// rmsb : 0001 which is 2s comp of number;
+// remove last bit form number and increase the conter
