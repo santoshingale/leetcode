88ba3b8dec624a5864688d7815a5d82bf3e9fd3c
santoshingale <58105046+santoshingale@users.noreply.github.com>
2025-03-08 16:43:28 +0530

    Time: 179 ms (70.21%) | Memory: 93.1 MB (66.48%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
@@ -1,53 +1,58 @@
 class WordDictionary {
 
-    class TrieNode{
-        TrieNode[] next = new TrieNode[26];
-        boolean isTerminal = false;
+    // This is not an easy problem.
+
+    class TrieNode {
+        TrieNode [] next = new TrieNode[26];
+        boolean isTerminal = false;        
     }
 
-    public TrieNode head = null;
-
+    TrieNode root; // global store? For? 
 
     public WordDictionary() {
-        head = new TrieNode();
+        root = new TrieNode();  // for lazy intialisation, and not eager initialisation        
     }
     
     public void addWord(String word) {
-        TrieNode root = head;
-        for(char c: word.toCharArray()){
-            int charIndex = c - 'a';
-            if(root.next[charIndex] == null) root.next[charIndex] = new TrieNode();
-            root = root.next[charIndex];
+        TrieNode curr = root;
+        for(char c: word.toCharArray()) {
+            if(curr.next[c - 'a'] == null) {
+                curr.next[c - 'a'] = new TrieNode(); // this TrieNode won't know it's next children
+            }
+            curr = curr.next[c - 'a']; // pointer shifting, to the node on line no. 18
         }
-        root.isTerminal = true;
+        curr.isTerminal = true;
     }
-
+    
     public boolean search(String word) {
-        return checkWord(word, 0, head);
+        return wildCardSearch(0, word, root);  
     }
 
-    private boolean checkWord(String word, int idx, TrieNode root){
-        if(idx >= word.length()) return root.isTerminal;
-        if(root == null) return false;
-        char c = word.charAt(idx);
-        if(c == '.'){
-            boolean b = false;
-            for(int i = 0; i < 26; i++){
-                if(root.next[i] != null){
-                    b = b || checkWord(word, idx + 1, root.next[i]);
+    private boolean wildCardSearch(int idx, String word, TrieNode curr) {
+        if(idx >= word.length()) {
+            return curr.isTerminal;
+        }
+
+        if(root == null) {
+            return false;
+        }
+
+        if(word.charAt(idx) == '.') {
+            for(int i = 0; i < 26; i++) {
+                if(curr.next[i] != null) {
+                    if(wildCardSearch(idx + 1, word, curr.next[i]))
+                        return true;
                 }
             }
-            return b;
-        }else{
-            if(root.next[c - 'a'] != null) return checkWord(word, idx + 1, root.next[c - 'a']);
-            else return false;
+            return false;
+        }
+        else {
+            if(curr.next[word.charAt(idx) - 'a'] != null) {
+                return wildCardSearch(idx + 1, word, curr.next[word.charAt(idx) - 'a']);
+            }
+            else {
+                return false;
+            }
         }
     }
 }
-
-/**
- * Your WordDictionary object will be instantiated and called as such:
- * WordDictionary obj = new WordDictionary();
- * obj.addWord(word);
- * boolean param_2 = obj.search(word);
- */
