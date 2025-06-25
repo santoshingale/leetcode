3a8a828e9159744d307b5a6da7b1c594dba27942
santoshingale <58105046+santoshingale@users.noreply.github.com>
2025-06-25 19:47:30 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (39.91%) - LeetSync

--- 670-maximum-swap/maximum-swap.java
+++ 670-maximum-swap/maximum-swap.java
@@ -3,20 +3,20 @@ class Solution {
         char[] nums = Integer.toString(num).toCharArray();
         int n = nums.length;
         int curMax = n - 1;
-        int leftMax = -1;
-        int rightMin = -1; 
+        int leftMin = -1;
+        int rightMax = -1; 
         for(int i = n - 2; i >= 0 ; i--){
             if(nums[curMax] < nums[i]){
                 curMax = i;
             }else if(nums[curMax] > nums[i]){
-                leftMax = curMax;
-                rightMin = i;
+                leftMin = curMax;
+                rightMax = i;
             }
         }
-        if(leftMax != -1){
-            char temp = nums[leftMax];
-            nums[leftMax] = nums[rightMin];
-            nums[rightMin] = temp;
+        if(leftMin != -1){
+            char temp = nums[leftMin];
+            nums[leftMin] = nums[rightMax];
+            nums[rightMax] = temp;
             return Integer.parseInt(new String(nums));
         }
         return num;
