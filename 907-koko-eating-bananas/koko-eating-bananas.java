212141ef8e8994e51f0c2d56f7c7d969ac954ea2
santoshingale <58105046+santoshingale@users.noreply.github.com>
2025-11-24 14:06:44 +0530

    Time: 26 ms (24.91%) | Memory: 47.8 MB (17.21%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -4,11 +4,11 @@ class Solution {
         int max = Integer.MIN_VALUE;
 
         for(int a : piles) {
-            max = Math.max(max,a);
+            max = Math.max(max, a);
         }
 
         while(min < max) {
-            int mid = (min+max)/2;
+            int mid = (min + max) / 2;
             if(getHoursToEat(piles, mid) > h) {
                 min = mid + 1;
             } else {
@@ -22,7 +22,7 @@ class Solution {
     private int getHoursToEat(int[] piles, int k) {
         int h = 0;
         for(int a : piles) {
-            h += Math.ceil((double)a/k);
+            h += Math.ceil((double)a / k);
         }
         return h;
     }
