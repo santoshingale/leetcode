8b7e7fdb1c8e29d5104a3d0eb8e146dd3273a8d2
santoshingale <58105046+santoshingale@users.noreply.github.com>
2026-04-25 11:25:02 +0530

    Time: 2 ms (100.00%) | Memory: 139.8 MB (44.80%) - LeetSync

--- 4018-minimum-moves-to-balance-circular-array/minimum-moves-to-balance-circular-array.java
+++ 4018-minimum-moves-to-balance-circular-array/minimum-moves-to-balance-circular-array.java
@@ -1,32 +1,45 @@
 class Solution {
     public long minMoves(int[] balance) {
-        long sum = 0; int idx = -1;
         int n = balance.length;
-        for(int i=0; i<n; i++){
+        long sum = 0;
+        int idx = -1;
+        long val = -1;
+        long result = 0;
+
+        for (int i = 0; i < n; i++) {
             sum += balance[i];
-            if(balance[i] < 0) idx = i;
+
+            if (balance[i] < 0) {
+                idx = i;
+                val = balance[i];
+            }
         }
-        if(sum < 0) return -1;
-        if(idx == -1) return 0;
-        long val = balance[idx];
-        int left = (idx-1+n) % n; int right = (idx+1) % n;
-        int i=1; long ans = 0; 
-        while(val < 0){
-            if(balance[left] > 0){
-                long take = Math.min(balance[left], -val);
+
+        if (sum < 0) return -1;
+        if (idx == -1) return 0;
+
+        int left = (idx - 1 + n) % n;
+        int right = (idx + 1) % n;
+        int dist = 1;
+
+        while (val < 0) {
+            if (balance[left] > 0) {
+                long take = Math.min((long) balance[left], -val);
                 val += take;
-                ans += take * i;
+                result += take * dist;
             }
 
-            if(val < 0 && balance[right] > 0){
-                long take2 = Math.min(balance[right], -val);
-                val += take2;
-                ans += take2 * i;
+            if (val < 0 && balance[right] > 0) {
+                long take1 = Math.min((long) balance[right], -val);
+                val += take1;
+                result += take1 * dist;
             }
+
             left = (left - 1 + n) % n;
             right = (right + 1) % n;
-            i++;
+            dist++;
         }
-        return ans;
+
+        return result;
     }
 }
